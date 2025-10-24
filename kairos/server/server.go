04f0560023b4f0c3660b485eb8c031379767cf93
nkxxll <niki.nice1203@gmail.com>
2025-10-24 16:20:45 +0200

    fix kairos server flag

--- kairos/server/server.go
+++ kairos/server/server.go
@@ -7,8 +7,9 @@ import (
 )
 
 func main() {
-	url := flag.String("u", "localhost:3000", "url for the server")
-	log.Println("Starting MCP server...")
+	url := flag.String("url", "localhost:3000", "url for the server")
+	flag.Parse()
+	log.Printf("Starting MCP server... at %s", *url)
 	if err := tmcp.RunServer(*url); err != nil {
 		log.Fatal(err)
 	}
